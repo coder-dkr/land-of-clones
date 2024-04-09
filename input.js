d39c0cce984cbb0e624ee66c87d9fa65caebad32
Dhruv Roy <dhruvakr9001@gmail.com>
2024-04-09 21:23:34 +0530

    made responsive,dark mode,footer

--- input.js
+++ input.js
@@ -1,24 +1,39 @@
 function optcome() {
-    document.getElementsByClassName('features')[0].classList.toggle('absolute');
-    document.getElementsByClassName('features')[0].classList.toggle('hamclcick');
-    document.getElementsByClassName('features')[0].classList.toggle('-translate-x-[50rem]');
+  document.getElementsByClassName('features')[0].classList.toggle('absolute');
+  document.getElementsByClassName('features')[0].classList.toggle('hamclcick');
+  document.getElementsByClassName('features')[0].classList.toggle('-translate-x-[50rem]');
 
 
 
 }
 
-let mybutton = document.getElementById("myBtnn");
+let toptton = document.getElementById("myBtnn");
 
-window.onscroll = function() {whenScroll()};
+window.onscroll = function () { whenScroll() };
 
 function whenScroll() {
   if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-    mybutton.style.display = "block";
+    toptton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    toptton.style.display = "none";
   }
 }
 function backTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+function goDark() {
+  document.getElementById('bodi').classList.toggle("bg-[#4C4646]");
+  document.getElementById('bodi').classList.toggle("text-yellow-500");
+  document.getElementById('heaven-moon').style.color ="white";
+
+}
+
+// var liz = document.getElementsByTagName('li');
+// liz.onmouseover = function(){
+//   const lis = liz.classList;
+// lis.add("unrline");
+// };
